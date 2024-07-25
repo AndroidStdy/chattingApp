83b7bfbc580098860ec438f3f38a110245de3f70
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-25 20:36:39 +0900

    사용자 목록 화면 - 하단탭 Fragment 적용

--- app/src/main/java/fastcampus/part2/chattingapp/MainActivity.kt
+++ app/src/main/java/fastcampus/part2/chattingapp/MainActivity.kt
@@ -6,14 +6,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import fastcampus.part2.chattingapp.databinding.ActivityMainBinding
+import fastcampus.part2.chattingapp.userlist.UserFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val userFragment = UserFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val currentUser = Firebase.auth.currentUser
         if(currentUser == null){
@@ -22,5 +30,32 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.userList ->{
+                    replaceFragment(userFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.chatroomList->{
+                    return@setOnItemSelectedListener true
+                }
+                R.id.myPage -> {
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    return@setOnItemSelectedListener false
+                }
+            }
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.frameLayout, fragment)
+                commit()
+            }
+    }
+
 }
