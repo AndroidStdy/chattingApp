25e457cb8a2c5275c3838988cc67432362171836
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-26 09:59:39 +0900

    채팅 기능 - 메시지 전송 기능 추가

--- app/src/main/java/fastcampus/part2/chattingapp/chatdetail/ChatActivity.kt
+++ app/src/main/java/fastcampus/part2/chattingapp/chatdetail/ChatActivity.kt
@@ -1,6 +1,7 @@
 package fastcampus.part2.chattingapp.chatdetail
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.Firebase
@@ -8,18 +9,20 @@ import com.google.firebase.auth.auth
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.MutableData
 import com.google.firebase.database.database
 import fastcampus.part2.chattingapp.Key
 import fastcampus.part2.chattingapp.databinding.ActivityChatdetailBinding
 import fastcampus.part2.chattingapp.userlist.UserItem
 
-class ChatActivity: AppCompatActivity() {
+class ChatActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityChatdetailBinding
+    private lateinit var binding: ActivityChatdetailBinding
 
     private var chatRoomId: String = ""
     private var otherUserId: String = ""
     private var myUserId: String = ""
+    private var myUserName: String = ""
 
     private val chatItemList = mutableListOf<ChatItem>()
 
@@ -28,15 +31,15 @@ class ChatActivity: AppCompatActivity() {
         binding = ActivityChatdetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        chatRoomId = intent.getStringExtra(EXTRA_CHAT_ROOM_ID)?: return
-        otherUserId = intent.getStringExtra(EXTRA_OTHER_USER_ID)?:return
-        myUserId = Firebase.auth.currentUser?.uid?:""
+        chatRoomId = intent.getStringExtra(EXTRA_CHAT_ROOM_ID) ?: return
+        otherUserId = intent.getStringExtra(EXTRA_OTHER_USER_ID) ?: return
+        myUserId = Firebase.auth.currentUser?.uid ?: ""
         val chatAdapter = ChatAdapter()
 
         Firebase.database.reference.child(Key.DB_USERS).child(myUserId).get()
             .addOnSuccessListener {
                 val myUserItem = it.getValue(UserItem::class.java)
-                val myUserName = myUserItem?.username
+                myUserName = myUserItem?.username ?: ""
             }
         Firebase.database.reference.child(Key.DB_USERS).child(otherUserId).get()
             .addOnSuccessListener {
@@ -46,41 +49,73 @@ class ChatActivity: AppCompatActivity() {
             }
 
         //채팅 가져오기
-        Firebase.database.reference.child(Key.DB_CHATS).child(chatRoomId).addChildEventListener(object : ChildEventListener{
-            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                val chatItem = snapshot.getValue(ChatItem::class.java)
-                chatItem ?: return
+        Firebase.database.reference.child(Key.DB_CHATS).child(chatRoomId)
+            .addChildEventListener(object : ChildEventListener {
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    val chatItem = snapshot.getValue(ChatItem::class.java)
+                    chatItem ?: return
 
-                chatItemList.add(chatItem)
-                chatAdapter.submitList(chatItemList)
-            }
+                    chatItemList.add(chatItem)
+                    chatAdapter.submitList(chatItemList)
+                }
 
-            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                // TODO: 수정 구현
-            }
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                    // TODO: 수정 구현
+                }
 
-            override fun onChildRemoved(snapshot: DataSnapshot) {
-                // TODO: 삭제 구현
-            }
+                override fun onChildRemoved(snapshot: DataSnapshot) {
+                    // TODO: 삭제 구현
+                }
 
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-                // TODO: 이동 구현
-            }
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                    // TODO: 이동 구현
+                }
 
-            override fun onCancelled(error: DatabaseError) {
-                // TODO: 에러 예외처리 구현
-            }
+                override fun onCancelled(error: DatabaseError) {
+                    // TODO: 에러 예외처리 구현
+                }
 
-        })
+            })
 
 
         binding.chatRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = chatAdapter
         }
+        binding.btnSend.setOnClickListener {
+            val message = binding.etMessage.text.toString()
 
+            //빈 메시지 전송 예외처리
+            if (message.isEmpty()) {
+                Toast.makeText(applicationContext, "빈 메시지를 전송할 수는 없습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val newChatItem = ChatItem(
+                message = message,
+                userId = myUserId,
+
+                )
+            //채팅 전송
+            Firebase.database.reference.child(Key.DB_CHATS).child(chatRoomId).push().apply {
+                newChatItem.chatId = key
+                setValue(newChatItem)
+            }
+
+            val updates: MutableMap<String, Any> = hashMapOf(
+                "${Key.DB_CHAT_ROOMS}/$myUserId/$otherUserId/lastMessage" to message,
+                "${Key.DB_CHAT_ROOMS}/$otherUserId/$myUserId/lastMessage" to message,
+                "${Key.DB_CHAT_ROOMS}/$otherUserId/$myUserId/chatRoomId" to chatRoomId,
+                "${Key.DB_CHAT_ROOMS}/$otherUserId/$myUserId/otherUserId" to myUserId,
+                "${Key.DB_CHAT_ROOMS}/$otherUserId/$myUserId/otherUserName" to myUserName,
+            )
+            Firebase.database.reference.updateChildren(updates)
+
+            binding.etMessage.text.clear()
+        }
     }
-    companion object{
+
+    companion object {
         const val EXTRA_CHAT_ROOM_ID = "CHAT_ROOM_ID"
         const val EXTRA_OTHER_USER_ID = "OTHER_USER_ID"
     }
