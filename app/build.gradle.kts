5b5341dfd762ac976575a1de9f029cbdc283ab10
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-07-31 10:35:42 +0900

    중복 META-INF/DEPENDENCIES 파일 문제 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,12 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    packaging{
+        resources{
+            excludes += "META-INF/DEPENDENCIES"
+        }
+
+    }
 }
 
 dependencies {
